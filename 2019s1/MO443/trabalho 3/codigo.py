b40581f935a1abb3accd8c8581c752bd05be7b2f
Gabriel <gabrielbianchin.gb@gmail.com>
2019-05-19 21:14:48 -0300

    trabalho 3 MO443

--- 2019s1/MO443/trabalho 3/codigo.py
+++ 2019s1/MO443/trabalho 3/codigo.py
@@ -2,12 +2,16 @@ import cv2
 import numpy as np
 import sys
 
-def funcao(imagem):
+
+def funcao(img):
 
 	#definicao dos kernels
-	kernel1 = np.ones((10, 1), np.uint8)
-	kernel2 = np.ones((1, 10), np.uint8)
-	kernel3 = np.ones((3, 1), np.uint8)
+	kernel1 = np.zeros((1, 100), np.uint8)
+	kernel2 = np.zeros((200, 1), np.uint8)
+	kernel3 = np.zeros((1, 30), np.uint8)
+
+	#transformar a imagem em fundo preto e texto em branco
+	img = cv2.bitwise_not(img)
 
 	#dilatacao seguida de erosao, conforme os passos (1) e (2)
 	img1 = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel1)
@@ -16,8 +20,7 @@ def funcao(imagem):
 	img2 = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel2)
 
 	#passo(5) - operador logico AND entre as duas imagens dos passos (2) e (4) 
-	img = np.logical_and(img1, img2)
-	img = np.array(img, dtype=np.uint8)
+	img = cv2.bitwise_and(img1, img2)
 
 	#passo(6) - fechamento da imagem do passo (5)
 	img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel3)
@@ -33,7 +36,15 @@ saida = sys.argv[2]
 #leitura da imagem em tons de cinza
 img = cv2.imread('imagens/bitmap.pbm', 0)
 
-img = funcao(img)
+imgsaida = funcao(img)
 
+nlabels, labels, stats, centroids = cv2.connectedComponentsWithStats(imgsaida)
 
-cv2.imwrite('saida/' + saida, img)
+for components in stats:
+	x = components[0]
+	y = components[1]
+	a = components[2]
+	b = components[3]
+	imgsaida = cv2.rectangle(imgsaida, (x, y), (x + a, y + b), 1, 5)
+
+cv2.imwrite('saida/' + saida, imgsaida)
