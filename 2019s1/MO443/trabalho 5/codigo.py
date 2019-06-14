3d64ff49310f69217c8ca01b07f58cc0c92dc700
Gabriel <gabrielbianchin.gb@gmail.com>
2019-06-14 12:47:02 -0300

    Trabalho 5 MO443 finalizado

--- 2019s1/MO443/trabalho 5/codigo.py
+++ 2019s1/MO443/trabalho 5/codigo.py
@@ -8,11 +8,14 @@ from time import time
 #diretorio da imagem
 dir_imagem = sys.argv[1]
 
+#metodo
+metodo = sys.argv[2]
+
 #numero de cores
-qtde_cores = int(sys.argv[2])
+qtde_cores = int(sys.argv[3])
 
 #nome da imagem de saida
-saida = sys.argv[3]
+saida = sys.argv[4]
 
 #leitura da imagem
 img = cv2.imread(dir_imagem)
@@ -22,26 +25,31 @@ w, h, d = img.shape
 assert d == 3
 image_array = np.reshape(img, (w * h, d))
 
-t0 = time()
-
 #clusterizacao com kmeans
-kmeans = KMeans(n_clusters = qtde_cores, random_state = 0).fit(image_array)
+if metodo == 'k-means++' or metodo == 'random':
 
-#predict os rotulos da imagem
-labels = kmeans.predict(image_array)
+	t0 = time()
 
-#salvando em um dicionario os centros
-codebook = kmeans.cluster_centers_
+	kmeans = KMeans(n_clusters = qtde_cores, init = metodo).fit(image_array)
 
-#reconstruindo a imagem
-d = codebook.shape[1]
-img_saida = np.zeros((w, h, d))
-label_idx = 0
-for i in range(w):
-	for j in range(h):
-		img_saida[i][j] = codebook[labels[label_idx]]
-		label_idx += 1
+	#predict os rotulos da imagem
+	labels = kmeans.predict(image_array)
 
-#salvando a imagem
-cv2.imwrite('saida/' + saida, img_saida)
-print("done in %0.3fs." % (time() - t0))
+	#salvando em um dicionario os centros
+	codebook = kmeans.cluster_centers_
+
+	#reconstruindo a imagem
+	d = codebook.shape[1]
+	img_saida = np.zeros((w, h, d))
+	label_idx = 0
+	for i in range(w):
+		for j in range(h):
+			img_saida[i][j] = codebook[labels[label_idx]]
+			label_idx += 1
+
+	#salvando a imagem
+	cv2.imwrite('saida/' + saida, img_saida)
+	print("done in %0.3fs." % (time() - t0))
+
+else:
+	print('Nao foi possivel aplicar a tecnica')
